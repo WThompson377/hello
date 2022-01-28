cb9428c7772467865a76f3fc2b5ff3aab766aa17
William Thompson <wthompson11@huskers.unl.edu>
2022-01-28 15:47:13 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: William Thompson
+ * Date: 2022/1/28
+ *
+ * A simple hello world program
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+     printf("William Thompson\n");
+     return 0;
+ }
